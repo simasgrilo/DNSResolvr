8164d1d22aa38ce8427aaf6395a2fc690496b69c
Erick <simasgrilo86@gmail.com>
2025-03-09 20:41:47 -0300

    Added basic logging features for the service

--- comp/server/server.py
+++ comp/server/server.py
@@ -1,34 +1,46 @@
-from flask import Flask
+from flask import Flask, request
 from comp.dns.DNSQuerier import DNSQuerier
 from comp.exc.BadServerNameException import BadServerNameException
+from comp.logging.Logger import Logger
+import os
 import json
 
 class DNSServer:
     __app = Flask(__name__)
+    
+    __logger = Logger(os.path.abspath(__file__) + "dns.log").logger
 
     HTTP_400_BAD_REQUEST = 400
 
     def __init__(self):
         self.__querier = DNSQuerier()
-        # print(querier.query_server("www.google.com"))
-        # print(querier.query_server("www.ic.uff.br"))
-        #print(self.__querier.query_server("www.amazon.com.br"))
 
-    def get_app(self):
+    @property
+    def app(self):
         return self.__app
 
-    def get_querier(self):
+    @property
+    def querier(self):
         return self.__querier
+    
+    @property
+    def logger(self):
+        return self.__logger
 
 
 dns = DNSServer()
-app = dns.get_app()
+app = dns.app
 
 @app.route("/dns/<address>")
 def get_dns(address: str):
     try:
-        return json.dumps(dns.get_querier().query_server(address))
+        extra_params = {
+            "client_ip": request.remote_addr,
+        }
+        dns.logger.info("Querying DNS server for address {}".format(address), extra=extra_params)
+        return json.dumps(dns.querier.query_server(address))
     except BadServerNameException as e:
+        dns.logger.error("Provided URL {} is a invalid URL".format(address), extra=extra_params)
         return incorrect_server_name_format(e)
 
 @app.errorhandler(400)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from comp.server.server import DNSServer
 
 
 def main():
-    server = DNSServer().get_app().run(host="0.0.0.0")
+    server = DNSServer().app.run(host="0.0.0.0")
 
 if __name__ == "__main__":
     main()
