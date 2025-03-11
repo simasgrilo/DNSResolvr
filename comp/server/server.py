01f1de86ee8d7c1960a77f7c375dd9c7fca782c3
Erick <simasgrilo86@gmail.com>
2025-03-11 01:19:49 -0300

    Added scheduling of the log flush towards a LogAggregator instance (if present) using threads

--- comp/server/server.py
+++ comp/server/server.py
@@ -5,12 +5,20 @@ from comp.logging import Logger, LogFlush
 from comp.utils.ConfigManager import ConfigManager
 import json
 import os
+import threading
 
 
 class DNSServer:
     __app = Flask(__name__)
     
+    __instance = None
+    
     HTTP_400_BAD_REQUEST = 400
+    
+    def __new__(cls, config_file: str):
+        if cls.__instance is None:
+            cls.__instance = super(DNSServer, cls).__new__(cls)
+        return cls.__instance
 
     def __init__(self, config_file: str):
         config_manager = ConfigManager(config_file)
@@ -21,7 +29,10 @@ class DNSServer:
         self.__logger.info("Starting DNS server with configuration from {}".format(config_file))
         self.__querier = DNSQuerier()
         self.__flusher = LogFlush(self._config)
-        self.__flusher.schedule_flush(self._log_path)
+        
+        # Schedule the log task sending in another thread so it will not block the execution of the server.
+        self._periodic_thread = threading.Thread(target=self.__flusher.schedule_flush, kwargs={"file_path" : self._log_path})
+        self._periodic_thread.start()
 
     @property
     def flask_app(self):
@@ -34,19 +45,23 @@ class DNSServer:
     @property
     def logger(self):
         return self.__logger
+    
+    def get_instance():
+        return DNSServer.__instance
 
 
     @__app.route("/dns/<address>")
-    def get_dns(self, address: str):
+    def get_dns(address: str):
+        dns = DNSServer.get_instance()
         try:
             extra_params = {
                 "client_ip": request.remote_addr,
             }
-            self.logger.info("Querying DNS server for address {}".format(address), extra=extra_params)
-            return json.dumps(self.querier.query_server(address))
+            dns.logger.info("Querying DNS server for address {}".format(address), extra=extra_params)
+            return json.dumps(dns.querier.query_server(address))
         except BadServerNameException as e:
-            self.logger.error("Provided URL {} is a invalid URL".format(address), extra=extra_params)
-            return self.incorrect_server_name_format(e)
+            dns.logger.error("Provided URL {} is a invalid URL".format(address), extra=extra_params)
+            return dns.incorrect_server_name_format(e)
 
     @__app.errorhandler(400)
     def incorrect_server_name_format(error):
